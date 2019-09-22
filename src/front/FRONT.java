7a7299d722206ac9960b50b3dbd5d7fec91a41f6
Darrel2018 <darrelp2000@gmail.com>
2019-09-22 15:48:55 +0200

    added header plus button mouse listeners

--- src/front/FRONT.java
+++ src/front/FRONT.java
@@ -14,6 +14,7 @@ public class FRONT {
 	private JFrame frame;
 	private Functions func;
 	
+	// Constructor.
 	public FRONT(){
 		
 		frame = new JFrame();
@@ -22,13 +23,14 @@ public class FRONT {
 		createView();
 	}
 	
+	// Creates GUI for frame and displays it.
 	private void createView(){
 		
 		frame.setLayout(null);
-		
 		frame.add(sidePanel());
 	}
 	
+	// Creates sidePanel.
 	private JPanel sidePanel(){
 		
 		int sidePanelWidth = 200;
@@ -37,16 +39,29 @@ public class FRONT {
 		
 		panel.setLayout(null);
 		panel.setBounds(0, 0, sidePanelWidth, HEIGHT);
-		panel.setBackground(func.setColor(192, 192, 192));
+		panel.setBackground(func.setColor(132, 132, 132));
 		
-		panel.add(func.createSidePanelButton(0, 150, sidePanelWidth, "res\\images\\select.png", "Select Music"));
-		panel.add(func.createSidePanelButton(0, 150 + (50 * 1 - 1), sidePanelWidth, "res\\images\\add.png", "Add Music"));
-		panel.add(func.createSidePanelButton(0, 150 + (50 * 2 - 2), sidePanelWidth, "res\\images\\remove.png", "Remove Music"));
-		panel.add(func.createSidePanelButton(0, 150 + (50 * 5 - 5), sidePanelWidth, "res\\images\\exit.png", "Exit"));
+		panel.add(func.createSidePanelHeader(0, 20, sidePanelWidth, 80));
+		
+		JPanel select_music = func.createSidePanelButton(0, 150, sidePanelWidth, "res\\images\\select.png", "Select Music");
+		JPanel add_music = func.createSidePanelButton(0, 150 + (50 * 1 - 1), sidePanelWidth, "res\\images\\add.png", "Add Music");
+		JPanel remove_music = func.createSidePanelButton(0, 150 + (50 * 2 - 2), sidePanelWidth, "res\\images\\remove.png", "Remove Music");
+		JPanel exit = func.createSidePanelButton(0, 150 + (50 * 5 - 5), sidePanelWidth, "res\\images\\exit.png", "Exit");
+		
+		func.addButtonListeners(select_music, "Select Music");
+		func.addButtonListeners(add_music, "Add Music");
+		func.addButtonListeners(remove_music, "Remove Music");
+		func.addButtonListeners(exit, "Exit");
+		
+		panel.add(select_music);
+		panel.add(add_music);
+		panel.add(remove_music);
+		panel.add(exit);
 		
 		return panel;
 	}
 	
+	//----====MAIN====----
 	public static void main(String[] args){
 		FRONT front = new FRONT();
 		
--- src/functions/Functions.java
+++ src/functions/Functions.java
@@ -2,19 +2,24 @@ package functions;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 
 public class Functions {
 	
+	// Returns a Color object.
 	public Color setColor(int r, int g, int b){
 		Color color = new Color(r, g, b);
 		return color;
 	}
 	
+	// Creates side-Panel-Buttons.
 	public JPanel createSidePanelButton(int x, int y, int sidePanelWidth, String iconPathLeft, String text){
 		
 		JPanel panel = new JPanel();
@@ -26,17 +31,18 @@ public class Functions {
 		panel.setBorder(BorderFactory.createLineBorder(setColor(255, 255, 255)));
 		
 		panel.add(createSideButtonIcon(0, 0, iconPathLeft));
-		panel.add(createTextLabel(70, 0, setColor(255, 255, 255), new Font("Segoe UI", 0, 12), text));
+		panel.add(createTextLabel(70, 0, setColor(255, 255, 255), new Font("Segoe UI", 0, 12), text, 78));
 		panel.add(createSideButtonIcon(170, 0, "res\\images\\next.png"));
 		
 		return panel;
 	}
 	
-	public JLabel createTextLabel(int x, int y, Color color, Font font, String text){
+	// Creates text-Labels.
+	public JLabel createTextLabel(int x, int y, Color color, Font font, String text, int textLabelWidth){
 		
 		JLabel label = new JLabel();
 		
-		label.setBounds(x, y, 78, 50);
+		label.setBounds(x, y, textLabelWidth, 50);
 		label.setText(text);
 		label.setForeground(color);
 		label.setFont(font);
@@ -44,6 +50,7 @@ public class Functions {
 		return label;
 	}
 	
+	// Creates side-Button-Icons.
 	private JLabel createSideButtonIcon(int x, int y, String path){
 		
 		JLabel iconLabel = new JLabel();
@@ -52,5 +59,60 @@ public class Functions {
 		iconLabel.setBounds(x, y, 50, 50);
 		
 		return iconLabel;
+	}
+	
+	// Creates side-Panel-Header.
+	public JPanel createSidePanelHeader(int x, int y, int width, int height){
+		
+		JPanel panel = new JPanel();
+		JSeparator sep = new JSeparator();
+		
+		panel.setLayout(null);
+		panel.setBounds(x, y, width, height);
+		panel.setBackground(setColor(132, 132, 132));
+		
+		sep.setBounds(0, height-2, width, 10);
+		sep.setForeground(setColor(255, 255, 255));
+		sep.setBackground(setColor(255, 255, 255));
+		
+		panel.add(sep);
+		panel.add(createTextLabel(17, 0, setColor(255, 255, 255), new Font("Segoe UI", 0, 30), "Music Player", 170));
+		
+		return panel;
+	}
+	
+	// adds listeners to buttons
+	public void addButtonListeners(JPanel panel, String buttonName){
+		
+		panel.addMouseListener(new MouseAdapter() {
+			
+			public void mousePressed(MouseEvent e){
+				System.out.println("Pressed " + buttonName);
+			}
+			
+			public void mouseReleased(MouseEvent e){
+				getButtonFunc(buttonName);
+			}
+		});
+	}
+	
+	// checks what button was pressed, then executes its following code.
+	private void getButtonFunc(String button){
+		
+		if(button.equals("Select Music")){
+			System.out.println("func1");
+		}
+		else if(button.equals("Add Music")){
+			System.out.println("func2");
+		}
+		else if(button.equals("Remove Music")){
+			System.out.println("func3");
+		}
+		else if(button.equals("Exit")){
+			System.out.println("func4");
+		}
+		else {
+			System.err.println("ERROR: Cannot find button func...");
+		}
 	}
 }
